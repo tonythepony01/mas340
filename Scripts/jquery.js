205eaf6126822187fefc7f5cabdb26435658539e
tonythepony01 <34711278+tonythepony01@users.noreply.github.com>
2018-05-10 22:12:00 +1000

    10512

--- Scripts/jquery.js
+++ Scripts/jquery.js
@@ -1,12 +1,12 @@
 function clickCheck() {
-		$('.clickable').click(function() {
+
 		$(this).fadeOut('fast');
 		var itemName = $(this).attr('id');
 		localStorage.setItem(itemName, 'true');
-		$('#inventorybutton').addClass('spring')
+		$('#inventorybutton').addClass('spring');
 		checkInventory();
 		$('#next').show();
-	});
+
 }
 	function HoverColor() {
 		$("#inventorybutton, #p1,#p2,#p3,#p4,#p5,#p6,#p7").hover(function(){
