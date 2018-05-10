ffd056d783912f8962bbf8753708a28bfa0f4306
tonythepony01 <34711278+tonythepony01@users.noreply.github.com>
2018-05-10 22:42:26 +1000

    10523

--- Scripts/jquery.js
+++ Scripts/jquery.js
@@ -1,18 +1,5 @@
 	
-	function clickCheck() {
-		$('.clickable').click(function() {
-			if ($('#inventorybutton').hasClass('spring')) {
-				$('#inventorybutton').removeClass('spring');
-				console.log($('#inventorybutton').hasClass('spring'))
-			}
-			$(this).fadeOut('fast');
-			var itemName = $(this).attr('id');
-			localStorage.setItem(itemName, 'true');
-			$('#inventorybutton').addClass('spring');
-			checkInventory();
-			$('#next').show();
-		});
-	}
+
 
 	function HoverColor() {
 		$("#inventorybutton, #p1,#p2,#p3,#p4,#p5,#p6,#p7").hover(function(){
