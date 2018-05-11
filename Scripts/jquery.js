096aaf147df6806ac836f50571757ddeeefffd06
tonythepony01 <34711278+tonythepony01@users.noreply.github.com>
2018-05-12 01:09:22 +1000

    110511

--- Scripts/jquery.js
+++ Scripts/jquery.js
@@ -36,9 +36,10 @@ function testColl() {
 			function checkCollision() {
 				var hit = $('.player').objectHitTest({"object": $('#tunnelbackground'),'transparency': true});
 				if(hit) {
+					$('.player').hide({effect:'explode'});
 					clearInterval(collisionTimer);
 					setTimeout(resetPlayer,1000);
-				}
+					}
 			}
 			function resetPlayer() {
 				$('.player').css({top:550,left: 350}).fadeIn();
